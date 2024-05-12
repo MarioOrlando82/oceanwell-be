4137f3a133d13bbb56fed92a70466fc019ffad09
Calvin Kurniawan <calvinfarrellinok@gmail.com>
2024-05-12 17:20:25 +0700

    feat: add app layout

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,6 @@
+<footer class="w-100 h-[10dvh] bg-[#01283C] py-5 text-center">
+    <div class="mx-auto w-fit">
+        <img src="{{ asset('Aset/Logo/OceanWell\'s Logo.png') }}" class="mx-auto w-[120px]" alt="logo">
+        <p class="w-fit text-white">© 2024 OceanWell All Rights Reserved</p>
+    </div>
+</footer>
---
+++ resources/views/layouts/navbar.blade.php
@@ -0,0 +1,17 @@
+<nav id="navbar" class="w-100 flex h-[10dvh] items-center justify-between bg-[#01283C] px-5 py-7">
+    <img src="{{ asset('Aset/Logo/OceanWell\'s Logo.png') }}" class="w-[120px] cursor-pointer" alt="logo">
+    <ul>
+        <li class="ms-4 inline-block list-none">
+            <x-nav-link href="#" :active="request()->routeIs('aboutus')">About Us</x-nav-link>
+        </li>
+        <li class="ms-4 inline-block list-none">
+            <x-nav-link href="#" :active="request()->routeIs('donate')">Donate</x-nav-link>
+        </li>
+        <li class="ms-4 inline-block list-none">
+            <x-nav-link href="#" :active="request()->routeIs('vounteer')">Volunteer</x-nav-link>
+        </li>
+        <li class="ms-4 inline-block list-none">
+            <x-nav-link href="#" :active="request()->routeIs('articles')">Articles</x-nav-link>
+        </li>
+    </ul>
+</nav>
