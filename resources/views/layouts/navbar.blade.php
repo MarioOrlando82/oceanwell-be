7e0096e5c47036243996e550ec346cb972ceba08
Calvin Farrellino Kurniawan <cavinfarrellinok@gmail.com>
2024-05-14 11:22:37 +0700

    feat: add logoout button

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -10,10 +10,27 @@
             <x-nav-link href="#" :active="request()->routeIs('donate')">Donate</x-nav-link>
         </li>
         <li class="ms-4 inline-block list-none">
-            <x-nav-link href="#" :active="request()->routeIs('vounteer')">Volunteer</x-nav-link>
+            <x-nav-link :href="route('index')" :active="request()->routeIs('index')">Volunteer</x-nav-link>
         </li>
         <li class="ms-4 inline-block list-none">
-            <x-nav-link href="#" :active="request()->routeIs('articles')">Articles</x-nav-link>
+            <x-nav-link :href="route('articles.index')" :active="request()->routeIs('articles.index')">Articles</x-nav-link>
         </li>
+        @if (!Auth::check())
+            <li class="ms-4 inline-block list-none">
+                <x-nav-link :href="route('login')" :active="request()->routeIs('login')">Login</x-nav-link>
+            </li>
+            <li class="ms-4 inline-block list-none">
+                <x-nav-link :href="route('register')" :active="request()->routeIs('login')">Register</x-nav-link>
+            </li>
+        @else
+            <li class="ms-4 inline-block list-none">
+                <form action="{{ route('logout') }}" method="POST">
+                    @csrf
+                    <x-nav-link :href="route('register')"
+                        onclick="event.preventDefault();
+                                                this.closest('form').submit();">{{ __('Log Out') }}</x-nav-link>
+                </form>
+            </li>
+        @endif
     </ul>
 </nav>
