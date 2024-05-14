2d20118802fdeaa117a4f9e4b74ab4a3f3009e77
Calvin Farrellino Kurniawan <cavinfarrellinok@gmail.com>
2024-05-14 14:03:36 +0700

    fix: change article seeder count

--- database/seeders/ArticleSeeder.php
+++ database/seeders/ArticleSeeder.php
@@ -13,6 +13,6 @@ class ArticleSeeder extends Seeder
      */
     public function run(): void
     {
-        Article::factory()->count(5)->create();
+        Article::factory()->count(40)->create();
     }
 }
